10a3e9a7bea05ad6e5346b6fa944f13201766f0d
iceonekot <rdsdahf@gmail.com>
2024-06-09 21:45:27 +0300

    bspwm folder
    folder which bspwm uses to work

---
+++ bspwm/autostart.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
+
+$HOME/.config/polybar/launch.sh &
+
+#change your keyboard if you need it
+setxkbmap -option grp:alt_shift_toggle us,ru
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
+else
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+fi
+
+feh --randomize --bg-fill ~/.wallpapers
+
+#dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+xsetroot -cursor_name left_ptr &
+
+# conky -c $HOME/.config/bspwm/system-overview &
+numlockx on &
+picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+copyq &
+
+#run volumeicon &
+#run variety &
+#nitrogen --restore &
+#run caffeine &
+#run firefox &
+#run thunar &
+#run discord &
+#run spotify &
+# blueberry-tray &
+# run nm-applet &
+# run pamac-tray &
